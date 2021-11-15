a1b2bbd6caa9087fa724cd19c3369ed727a26c55
Yulia-nia <sylianetttogggg@gmail.com>
2021-11-15 20:29:54 +0300

    Добавьте файлы проекта.

---
+++ FanficMobileVersion/FanficMobileVersion/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using FanficMobileVersion.ViewModels;
+using FanficMobileVersion.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FanficMobileVersion
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
